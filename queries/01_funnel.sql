bd09bab6659bb0557d4d2d0bfda171ea91e6b3da
Alena Merkulova <alena.merkulova.2022@gmail.com>
2026-07-01 13:51:25 +0300

    01_funnel.sql

--- queries/01_funnel.sql
+++ queries/01_funnel.sql
@@ -1,1 +1,37 @@
+select
+	'app_install' as step,
+	count (distinct user_id) as users
+from events
+where event_type = 'app_install'
 
+union all 
+
+select
+	'registration_complete' as step,
+	count (distinct user_id) as users
+from events 
+where event_type = 'registration_complete'
+
+union all
+
+select
+	'onboarding_complete' as step,
+	count (distinct user_id) as users
+from events 
+where event_type = 'onboarding_complete'
+
+union all
+
+select
+	'trial_activated' as step,
+	count (distinct user_id) as users
+from events 
+where event_type = 'trial_activated'
+
+union all
+
+select
+	'purchase' as step,
+	count (distinct user_id) as users
+from events 
+where event_type = 'purchase';
