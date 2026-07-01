5e4e2ab7df438ec558fc9706138c3955c3884b6c
Alena Merkulova <alena.merkulova.2022@gmail.com>
2026-07-01 14:08:14 +0300

    02_ios_vs_android.sql

--- queries/02_ios_vs_android.sql
+++ queries/02_ios_vs_android.sql
@@ -1,1 +1,26 @@
-
+with installs_calculation as(
+	select
+		u.platform,
+		count(distinct u.user_id) as installs
+	from users u 
+	join events e on e.user_id = u.user_id 
+	where e.event_type = 'app_install'
+	group by u.platform
+  ),
+	
+purchase_calculation as(
+	select
+		u.platform,
+		count(distinct u.user_id) as purchase
+	from users u 
+	join events e on e.user_id = u.user_id
+	where e.event_type = 'purchase'
+	group by u.platform)
+  
+select
+	pc.platform,
+	ic.installs,
+	pc.purchase,
+	round(((1.0*pc.purchase/ic.installs)*100),2) as cr
+from purchase_calculation pc
+join installs_calculation ic on pc.platform = ic.platform;
